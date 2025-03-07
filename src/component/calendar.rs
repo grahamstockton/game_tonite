5a151d6b550338c5b0396b73ab31016cd32dea69
graham <grahamstockton50@gmail.com>
2025-03-06 19:05:02 -0800

    remove compiled css file

--- src/component/calendar.rs
+++ src/component/calendar.rs
@@ -115,6 +115,14 @@ pub fn Calendar() -> impl IntoView {
                 </div>
             </div>
         </div>
+        <div class="badge badge-primary">Primary</div>
+        <div class="badge badge-secondary">Secondary</div>
+        <div class="badge badge-accent">Accent</div>
+        <div class="badge badge-neutral">Neutral</div>
+        <div class="badge badge-info">Info</div>
+        <div class="badge badge-success">Success</div>
+        <div class="badge badge-warning">Warning</div>
+        <div class="badge badge-error">Error</div>
     }
 }
 
